a900afd2ce67edfa1a4c0783677d38e32ab10540
Fahmi Fauzi Rahman <fahmifauzirahman@gmail.com>
2024-10-17 08:27:21 +0700

    grouping routes

--- routes/web.php
+++ routes/web.php
@@ -3,39 +3,20 @@
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-// Auth
-Route::get('login', [AuthenticatedSessionController::class, 'create'])
-    ->name('login')
-    ->middleware('guest');
+Route::delete('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
 
-Route::post('login', [AuthenticatedSessionController::class, 'store'])
-    ->name('login.store')
-    ->middleware('guest');
+Route::middleware('guest')->group(function () {
+    // Auth
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('register', [RegisteredUserController::class, 'store'])->name('register.store');
+});
 
-Route::get('register', [RegisteredUserController::class, 'create'])
-    ->name('register')
-    ->middleware('guest');
-
-Route::post('register', [RegisteredUserController::class, 'store'])
-    ->name('register.store')
-    ->middleware('guest');
-
-Route::delete('logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->name('logout');
-
-// Dashboard
-
-Route::get('/', [DashboardController::class, 'index'])
-    ->name('dashboard')
-    ->middleware('auth');
-
-Route::get('/about', function () {
-    return inertia('AboutPage');
-})->middleware('auth');
-
-
-// Route::get('/register', function () {
-//     return inertia('auth/RegisterPage');
-// });
+Route::middleware('auth')->group(function() {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/about', [PageController::class, 'about'])->name('about');
+});
